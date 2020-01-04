7066a0f64d524ea1bb2dee871c0de8d45424e787
AdrianMartinezT <59429351+AdrianMartinezT@users.noreply.github.com>
2020-01-03 22:05:11 -0600

    Update validacion.php
    Se quito lo comentado y se modifico el último if, haciendo comparación y no comparación

--- validacion.php
+++ validacion.php
@@ -3,29 +3,6 @@ function index(){
 	window.location="inicio.php";}
 	</script>
 <?php  
-/*session_start();
-$user    = $_POST['user'];
-$pass    = $_POST['pass'];
-
-$abrir=fopen("data.txt","r") or die ("Error - No fue posible abrir el archivo");
-
-while ($linea=fgets($abrir))
-{
- $partes=explode('|',trim($linea));
- }
-if (($user==$partes[0]) && ($pass==$partes[1]))
-{
-	echo 'hola';
-//header('location:inicio.php');
-}
-else {
-	echo 'error';   
-      /*  echo "Nombre de usuario o contraseña inválidos";
-		echo "Vuelve a intentarlo";
-		include 'login.php';
-}
-
-fclose("data.txt");*/
 
 session_start();
 $user    = $_POST['user'];
